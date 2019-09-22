c961d3be5c7ad9575d8ff8bd5710d5295dc38d32
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2019-09-22 14:19:06 +0200

    NoteActivity Initialization and Swiping list items to remove

--- app/src/main/java/com/android/notes/NotesListActivity.java
+++ app/src/main/java/com/android/notes/NotesListActivity.java
@@ -1,11 +1,15 @@
 package com.android.notes;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import com.android.notes.adapters.NotesRecyclerAdapter;
 import com.android.notes.interfaces.OnNoteListener;
@@ -19,6 +23,7 @@ public class NotesListActivity extends AppCompatActivity implements OnNoteListen
 
     // Ui components
     private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
 
     // vars
     private ArrayList<Note> mNotes = new ArrayList<>();
@@ -29,6 +34,15 @@ public class NotesListActivity extends AppCompatActivity implements OnNoteListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
         mRecyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotesListActivity.this, NoteActivity.class);
+                startActivity(intent);
+            }
+        });
 
         setSupportActionBar((Toolbar) findViewById(R.id.notes_toolbar));
         setTitle("Notes");
@@ -51,6 +65,7 @@ public class NotesListActivity extends AppCompatActivity implements OnNoteListen
     private void initRecyclerView() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+        new ItemTouchHelper(itemTouch).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes, this);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
@@ -60,5 +75,22 @@ public class NotesListActivity extends AppCompatActivity implements OnNoteListen
         Intent intent = new Intent(this, NoteActivity.class);
         intent.putExtra("selected_note", mNotes.get(position));
         startActivity(intent);
+    }
+
+    private ItemTouchHelper.SimpleCallback itemTouch = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
+
+    private void deleteNote(Note note) {
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
     }
 }
